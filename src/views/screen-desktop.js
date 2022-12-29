5b0dc7929e2a51f2815c3ea3c263472164e86fac
RafNik1234 <121479057+RafNik1234@users.noreply.github.com>
2022-12-29 02:39:31 -0500

    Commit from TeleportHQ

---
+++ src/views/screen-desktop.js
@@ -0,0 +1,168 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './screen-desktop.css'
+
+const ScreenDesktop = (props) => {
+  return (
+    <div className="screen-desktop-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="screen-desktop-screen-desktop">
+        <div className="screen-desktop-section-headline">
+          <div className="screen-desktop-heading">
+            <span className="screen-desktop-text H3-WorkSans">
+              <span>Trending Collection</span>
+            </span>
+          </div>
+          <div className="screen-desktop-subhead">
+            <span className="screen-desktop-text02 BodyText-WorkSans">
+              <span>Checkout our weekly updated trending collection.</span>
+            </span>
+          </div>
+        </div>
+        <div className="screen-desktop-collection-card-row">
+          <div className="screen-desktop-collection-card">
+            <div className="screen-desktop-photos-numberofadditional-n-f-ts">
+              <img
+                alt="PrimaryPhotoPlaceholderI1342"
+                src="/playground_assets/primaryphotoplaceholderi1342-phk-400h.png"
+                className="screen-desktop-primary-photo-placeholder"
+              />
+              <div className="screen-desktop-frame228">
+                <img
+                  alt="SecondaryPhotoPlaceholderI1342"
+                  src="/playground_assets/secondaryphotoplaceholderi1342-j5za-200h.png"
+                  className="screen-desktop-secondary-photo-placeholder"
+                />
+                <img
+                  alt="SecondaryPhotoPlaceholderI1342"
+                  src="/playground_assets/secondaryphotoplaceholderi1342-gaig-200h.png"
+                  className="screen-desktop-secondary-photo-placeholder1"
+                />
+                <div className="screen-desktop-numberofadditional-n-f-ts">
+                  <span className="screen-desktop-text04 H5-SpaceMono">
+                    <span>1025+</span>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="screen-desktop-collection-info">
+              <span className="screen-desktop-text06 H5-WorkSans">
+                <span>DSGN Animals</span>
+              </span>
+              <div className="screen-desktop-artist-card">
+                <div className="screen-desktop-avatar">
+                  <div className="screen-desktop-asset122">
+                    <img
+                      alt="AvatarPlaceholderI1342"
+                      src="/playground_assets/avatarplaceholderi1342-hcug-200h.png"
+                      className="screen-desktop-avatar-placeholder"
+                    />
+                  </div>
+                </div>
+                <span className="screen-desktop-text08 Base(Body)-WorkSans">
+                  <span>MrFox</span>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="screen-desktop-collection-card1">
+            <div className="screen-desktop-photos-numberofadditional-n-f-ts1">
+              <img
+                alt="PrimaryPhotoPlaceholderI1342"
+                src="/playground_assets/primaryphotoplaceholderi1342-pxml-400h.png"
+                className="screen-desktop-primary-photo-placeholder1"
+              />
+              <div className="screen-desktop-frame2281">
+                <img
+                  alt="SecondaryPhotoPlaceholderI1342"
+                  src="/playground_assets/secondaryphotoplaceholderi1342-nz2-200h.png"
+                  className="screen-desktop-secondary-photo-placeholder2"
+                />
+                <img
+                  alt="SecondaryPhotoPlaceholderI1342"
+                  src="/playground_assets/secondaryphotoplaceholderi1342-ipbb-200h.png"
+                  className="screen-desktop-secondary-photo-placeholder3"
+                />
+                <div className="screen-desktop-numberofadditional-n-f-ts1">
+                  <span className="screen-desktop-text10 H5-SpaceMono">
+                    <span>1025+</span>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="screen-desktop-collection-info1">
+              <span className="screen-desktop-text12 H5-WorkSans">
+                <span>Magic Mushrooms</span>
+              </span>
+              <div className="screen-desktop-artist-card1">
+                <div className="screen-desktop-avatar1">
+                  <div className="screen-desktop-asset1221">
+                    <img
+                      alt="AvatarPlaceholderI1342"
+                      src="/playground_assets/avatarplaceholderi1342-tepb-200h.png"
+                      className="screen-desktop-avatar-placeholder1"
+                    />
+                  </div>
+                </div>
+                <span className="screen-desktop-text14 Base(Body)-WorkSans">
+                  <span>Shroomie</span>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="screen-desktop-collection-card2">
+            <div className="screen-desktop-photos-numberofadditional-n-f-ts2">
+              <img
+                alt="PrimaryPhotoPlaceholderI1342"
+                src="/playground_assets/primaryphotoplaceholderi1342-vaxi-400h.png"
+                className="screen-desktop-primary-photo-placeholder2"
+              />
+              <div className="screen-desktop-frame2282">
+                <img
+                  alt="SecondaryPhotoPlaceholderI1342"
+                  src="/playground_assets/secondaryphotoplaceholderi1342-bhl-200h.png"
+                  className="screen-desktop-secondary-photo-placeholder4"
+                />
+                <img
+                  alt="SecondaryPhotoPlaceholderI1342"
+                  src="/playground_assets/secondaryphotoplaceholderi1342-9817-200h.png"
+                  className="screen-desktop-secondary-photo-placeholder5"
+                />
+                <div className="screen-desktop-numberofadditional-n-f-ts2">
+                  <span className="screen-desktop-text16 H5-SpaceMono">
+                    <span>1025+</span>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="screen-desktop-collection-info2">
+              <span className="screen-desktop-text18 H5-WorkSans">
+                <span>Disco Machines</span>
+              </span>
+              <div className="screen-desktop-artist-card2">
+                <div className="screen-desktop-avatar2">
+                  <div className="screen-desktop-asset1222">
+                    <img
+                      alt="AvatarPlaceholderI1342"
+                      src="/playground_assets/avatarplaceholderi1342-7yr-200h.png"
+                      className="screen-desktop-avatar-placeholder2"
+                    />
+                  </div>
+                </div>
+                <span className="screen-desktop-text20 Base(Body)-WorkSans">
+                  <span>BeKind2Robots</span>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ScreenDesktop
